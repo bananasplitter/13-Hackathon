2de05828d14c40b7d8dc018510347c2befa9e435
bananasplitter <tiana_pehle@yahoo.com>
2016-01-31 03:47:30 -0800

    Pokedex Hackathon

---
+++ Pokedex/Pokedex/Domain/Pokemon.core.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokedex
+{
+    public class Pokemon
+    {
+        public string name { get; set; }
+        public string pokemon { get; set; }
+        public string resource_uri { get; set; }
+        public string sp_atk { get; set; }
+        public string sp_def { get; set; }
+        public string speed { get; set; }
+        public string types { get; set; } 
+    }
+}
---
+++ Pokedex/Pokedex/PokemonInfo.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Pokedex
+{
+    /// <summary>
+    /// Interaction logic for PokemonInfo.xaml
+    /// </summary>
+    public partial class PokemonInfo : Window
+    {
+        public PokemonInfo()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonGetPokemon_Click(object sender, RoutedEventArgs e)
+        {
+            var pokemon = new Pokemon
+            {
+                pokemon = textBoxPokemonName
+
+ 
+            };
+
+        }
+    }
+}
